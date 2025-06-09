896137f062ee522921b97e1093ec4c1a7ef7e133
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-09 22:57:16 +0900

    --config 설정

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+from dataclasses import asdict
+
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
-def main():
-    return "hello world"
+from common.config import conf
+
+def create_app():
+
+    c = conf()
+    app = FastAPI()
+
+    conf_dict = asdict(c)
+    db.init_app(app, **conf_dict)
+    # 데이터 베이스 이니셜라이즈
+    # 레디스 이니셜라이즈
+    # 미들웨어 정의
+    # 라우터 정의
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #reload=conf().PROJ_RELOAD
+
+# docker로 빌드를 한다...?
