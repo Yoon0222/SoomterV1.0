357b22bf5a3ebd16482dd2de8d44e8d85aba8fa4
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-10 14:30:00 +0900

    -- cicd commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def create_app():
 
     return app
 
-app = create_app()
+app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #reload=conf().PROJ_RELOAD
