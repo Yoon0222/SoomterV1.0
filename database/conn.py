a213f5cd77e7a96a4f0ac2bed85c83e0b70aa315
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 17:59:56 +0900

    -- 자동화 배포 진행

--- database/conn.py
+++ database/conn.py
@@ -1,86 +1,10 @@
-# from fastapi import FastAPI
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# import logging
-#
-# class SQLAlchemy:
-#     def __init__(self, app: FastAPI = None, **kwargs):
-#         self._engine = None
-#         self._session = None
-#         if app is not None:
-#             self.init_app(app=app, **kwargs)
-#
-#     def init_app(self, app: FastAPI, **kwargs):
-#         """
-#         DB초기화 함수
-#         :param app: FastAPI 인스터스
-#         :param kwargs:
-#         :return:
-#         """
-#         database_url = kwargs.get("DB_URL")
-#         pool_recycle = kwargs.setdefault("DB_POOL_RECYCLE", 900)
-#         echo = kwargs.setdefault("DB_ECHO", True)
-#
-#         self._engine = create_engine(
-#             database_url,
-#             echo=echo,
-#             pool_recycle=pool_recycle,
-#             pool_pre_ping=True
-#         )
-#         self._session = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
-#
-#         @app.on_event("startup")
-#         def startup():
-#             self._engine.connect()
-#             logging.info("DB CONNECTED")
-#
-#         @app.on_event("shutdown")
-#         def shutdown():
-#             self._session.close_all()
-#             self._engine.dispose()
-#             logging.info("DB DISCONNECTED")
-#
-#     def get_db(self):
-#
-#         """
-#         요청마다 DB 세션 유지 함수
-#         :return:
-#         """
-#         if self._session is None:
-#             raise Exception("MUST BE CALLED 'init_app")
-#         db_session = None
-#         try:
-#             db_session = self._session()
-#             yield db_session
-#         finally:
-#             db_session.close()
-#
-#     @property
-#     def session(self):
-#         return self.get_db()
-#     @property
-#     def engine(self):
-#         return self._engine
-#
-# db = SQLAlchemy()
-# Base = declarative_base()
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from config import DB_NAME,DB_PORT,DB_USER,DB_HOST,DB_PASSWORD
 
-user_name = "root"
-user_pwd = "dbstlr23"
-db_host = "127.0.0.1"
-db_name = "soomter"
 
-DATABASE = 'mysql:%s/%s@%s/%s?charset=utf8' % (
-    user_name,
-    user_pwd,
-    db_host,
-    db_name
-)
+DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
 ENGINE = create_engine(
     DATABASE,
