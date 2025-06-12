7f4a7c54468227f89eb372f0819cf2c71cf0baee
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 19:07:14 +0900

    -- cicd 테스트

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ register_exception_handlers(app)
 
 app.include_router(user_router, prefix="/user", tags=["Users"])
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #reload=conf().PROJ_RELOAD
