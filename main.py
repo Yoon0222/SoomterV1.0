3ea9b39f4a9c5207c9cf8ee09a93b31b4275d0b7
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 20:09:58 +0900

    ______

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 register_exception_handlers(app)
 
 app.include_router(user_router, prefix="/user", tags=["Users"])
-##
+####
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #reload=conf().PROJ_RELOAD
