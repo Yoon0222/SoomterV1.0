e0d7a30fc41c56ca156f6fa2e8748d6d9f9a4b75
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 23:51:58 +0900

    -- 테스트 설정 삭제

--- router/users_router.py
+++ router/users_router.py
@@ -9,21 +9,6 @@ router = APIRouter()
 def test():
     return {"test": "test"}
 
-@router.post("/aaUser", response_model=APIResponse)
-def aaUser(name: str, ):
-    return make_response(code=400, error="Fail")
-
-@router.post("/bbUser", response_model=APIResponse)
-def bbUser(name: str, ):
-    return make_response(code=400, error="Fail")
-@router.post("/ccUser", response_model=APIResponse)
-def ccUser(name: str, ):
-    return make_response(code=400, error="Fail")
-
-@router.post("/ddUser", response_model=APIResponse)
-def ddUser(name: str, ):
-    return make_response(code=400, error="Fail")
-
 
 @router.post("/newUser", response_model=APIResponse)
 def newUser(name: str, ):
