23940e470ad9ecdab4ca0f5ce19162ccf01679d6
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 19:17:39 +0900

    cicd 결과확인

--- router/users_router.py
+++ router/users_router.py
@@ -9,6 +9,10 @@ router = APIRouter()
 def test():
     return {"test": "test"}
 
+@router.post("/aaUser", response_model=APIResponse)
+def newUser(name: str, ):
+    return make_response(code=400, error="Fail")
+
 @router.post("/newUser", response_model=APIResponse)
 def newUser(name: str, ):
 
