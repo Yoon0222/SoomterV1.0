276c4186348f181b458d7d9ae38cfaf6279b4dd1
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-12 19:13:07 +0900

    -- cicd 테스트

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 from router.users_router import router as user_router
 from utils.exception import register_exception_handlers
+
 app = FastAPI()
+
 register_exception_handlers(app)
+
 app.include_router(user_router, prefix="/user", tags=["Users"])
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #reload=conf().PROJ_RELOAD
