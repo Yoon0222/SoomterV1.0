14d11f5e4229a0b784c2d08ee6c790c53f859b2b
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-16 16:08:18 +0900

    -- db 연결

--- database/conn.py
+++ database/conn.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from database.config import DB_NAME, DB_PORT, DB_USER, DB_HOST, DB_PASSWORD
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+# DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+DATABASE_URL = f"mysql+mysqlconnector://root:dbstlr23@172.31.37.150:3306/soomter?charset=utf8mb4"
+
+
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
