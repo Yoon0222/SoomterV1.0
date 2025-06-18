0ba2efc23919f1e733d5f380e0018f64553d8293
Yoon <troy.yoonsik.shin@gmail.com>
2025-06-18 16:39:23 +0900

    -- api 개발 진행 중

--- router/map_router.py
+++ router/map_router.py
@@ -12,6 +12,6 @@ from service import map_service
 
 router = APIRouter()
 
-@router.get("/getLocation")
-def getLocation(loc : Location, db: Session = Depends(get_db)):
-    return APIResponse
+@router.get("/getLocations")
+def getLocations(loc : Location, db: Session = Depends(get_db)):
+    return map_service.getLocations(loc, db)
--- service/map_service.py
+++ service/map_service.py
@@ -2,4 +2,28 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from model.code import fail, success
 
+from model.map import LocationTable
+
 from utils.response import make_response
+
+from math import pi, cos, sin, sqrt, atan2, radians
+
+
+def getLocations(loc, db):
+    #전달받은 위도 경도를 통해 전방 1KM 지역에 있는 흡연구역 위도경도를 리턴
+
+    R = 6371
+    delta_lat = (1.0 / R) * (180 / pi)
+    delta_lng = delta_lat / cos(radians(loc.Latitude))
+
+    min_lat = loc.Latitude - delta_lat
+    max_lat = loc.Latitude + delta_lat
+    min_lng = loc.Longitude - delta_lng
+    max_lng = loc.Longitude + delta_lng
+
+    candidates = db.query(LocationTable).filter(
+        LocationTable.Latitude.between(min_lat, max_lat),
+        LocationTable.Longitude.between(min_lng, max_lng)
+    ).all()
+
+    return make_response()
